c962b3e1aea8a64bf2c4f8f90644f064afa45ec4
Swapnil <swapnilpingale22@gmail.com>
2023-09-01 15:21:48 +0530

    first eg complete

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'models/users_model.dart';
@@ -21,6 +22,7 @@ class _HomepageState extends State<Homepage> {
   Future<List<UsersModel>> getUserData() async {
     final responce =
         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
     var data = jsonDecode(responce.body);
 
     if (responce.statusCode == 200) {
@@ -40,39 +42,48 @@ class _HomepageState extends State<Homepage> {
         title: const Text("API Data"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
+      body: RefreshIndicator(
+        onRefresh: () async => await getUserData(),
+        child: Column(
+          children: [
+            Expanded(
               child: FutureBuilder(
-            future: getUserData(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var user = snapshot.data;
-                return ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: userList.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                        child: ListTile(
-                      title: Text(user![index].name),
-                      subtitle: Text(user[index].email),
-                      isThreeLine: true,
-                      leading:
-                          CircleAvatar(child: Text(user[index].id.toString())),
-                    ));
-                  },
-                );
-              } else {
-                const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          )),
-        ],
+                future: getUserData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    var user = snapshot.data;
+                    return ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: userList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.all(8),
+                            leading: CircleAvatar(
+                                child: Text(user![index].id.toString())),
+                            title: Text('Name: ${user[index].name}'),
+                            subtitle: Text('Email: ${(user[index].email)}'),
+                            trailing: Column(
+                              children: [
+                                const Text('Geo Lat/Lng:'),
+                                Text(user[index].address.geo.lat),
+                                Text(user[index].address.geo.lng),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: CupertinoActivityIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unused_import
 
+import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'page_2.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
       title: "Api Practice",
-      home: const Homepage(),
+      home: const SecondPage(),
     );
   }
 }
